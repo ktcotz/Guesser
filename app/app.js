66384b263f30d816ad038dc3418ee68141eba650
ktcotz <naskret.kamil@gmail.com>
2024-06-02 00:39:59 +0200

    feat: guess functionality
    Work around guess functionality, mostly work with validation.

---
+++ app/app.js
@@ -0,0 +1,48 @@
+var Errors;
+(function (Errors) {
+    Errors["NOT_FOUND_GUESS_INPUT"] = "Can't find a guess input \uD83D\uDED1";
+    Errors["EMPTY_INPUT"] = "Empty input \uD83D\uDED1";
+    Errors["INVALID_INPUT"] = "Accept numbers between 1-20 \uD83D\uDED1";
+})(Errors || (Errors = {}));
+var config = {
+    MIN: 1,
+    MAX: 20,
+};
+var handleGuesserGame = function () {
+    var messageElement = document.querySelector("[data-message]");
+    var guessInput = document.querySelector("[data-guess]");
+    var checkButton = document.querySelector("[data-check]");
+    var getGuessInputNumber = function () {
+        if (!guessInput) {
+            throw new Error(Errors.NOT_FOUND_GUESS_INPUT);
+        }
+        var number = Number(guessInput.value);
+        if (!number) {
+            throw new Error(Errors.EMPTY_INPUT);
+        }
+        if (number <= config.MIN || number >= config.MAX) {
+            throw new Error(Errors.INVALID_INPUT);
+        }
+        return number;
+    };
+    var setMessage = function (message) {
+        if (!messageElement)
+            return;
+        messageElement.textContent = message;
+    };
+    var guessNumber = function () {
+        try {
+            var guessNumber_1 = getGuessInputNumber();
+            console.log(guessNumber_1);
+        }
+        catch (err) {
+            if (err instanceof Error) {
+                setMessage(err.message);
+            }
+        }
+    };
+    checkButton === null || checkButton === void 0 ? void 0 : checkButton.addEventListener("click", function () {
+        guessNumber();
+    });
+};
+handleGuesserGame();
---
+++ app/app.ts
@@ -0,0 +1,58 @@
+enum Errors {
+  NOT_FOUND_GUESS_INPUT = "Can't find a guess input 🛑",
+  EMPTY_INPUT = "Empty input 🛑",
+  INVALID_INPUT = "Accept numbers between 1-20 🛑",
+}
+
+const config = {
+  MIN: 1,
+  MAX: 20,
+};
+
+const handleGuesserGame = () => {
+  const messageElement =
+    document.querySelector<HTMLSpanElement>("[data-message]");
+  const guessInput = document.querySelector<HTMLInputElement>("[data-guess]");
+  const checkButton = document.querySelector<HTMLButtonElement>("[data-check]");
+
+  const getGuessInputNumber = () => {
+    if (!guessInput) {
+      throw new Error(Errors.NOT_FOUND_GUESS_INPUT);
+    }
+
+    const number = Number(guessInput.value);
+
+    if (!number) {
+      throw new Error(Errors.EMPTY_INPUT);
+    }
+
+    if (number <= config.MIN || number >= config.MAX) {
+      throw new Error(Errors.INVALID_INPUT);
+    }
+
+    return number;
+  };
+
+  const setMessage = (message: string) => {
+    if (!messageElement) return;
+
+    messageElement.textContent = message;
+  };
+
+  const guessNumber = () => {
+    try {
+      const guessNumber = getGuessInputNumber();
+      console.log(guessNumber);
+    } catch (err) {
+      if (err instanceof Error) {
+        setMessage(err.message);
+      }
+    }
+  };
+
+  checkButton?.addEventListener("click", () => {
+    guessNumber();
+  });
+};
+
+handleGuesserGame();
