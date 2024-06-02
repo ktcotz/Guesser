f7da70016e9367ae47a660ebdbc12a6ce4ca13c4
ktcotz <naskret.kamil@gmail.com>
2024-06-02 10:38:42 +0200

    feat: guess game
    Work around guess game functionality.

--- app/app.js
+++ app/app.js
@@ -3,15 +3,18 @@ var Errors;
     Errors["NOT_FOUND_GUESS_INPUT"] = "Can't find a guess input \uD83D\uDED1";
     Errors["EMPTY_INPUT"] = "Empty input \uD83D\uDED1";
     Errors["INVALID_INPUT"] = "Accept numbers between 1-20 \uD83D\uDED1";
+    Errors["NUMBER_TO_LOW"] = "Too low!";
+    Errors["NUMBER_TO_HIGH"] = "Too high!";
+    Errors["END_GAME"] = "Game just finished!";
 })(Errors || (Errors = {}));
 var config = {
     MIN: 1,
     MAX: 20,
 };
 var handleGuesserGame = function () {
-    var messageElement = document.querySelector("[data-message]");
-    var guessInput = document.querySelector("[data-guess]");
-    var checkButton = document.querySelector("[data-check]");
+    var generateRandomNumber = function (min, max) {
+        return Math.trunc(Math.random() * (max - min) + min);
+    };
     var getGuessInputNumber = function () {
         if (!guessInput) {
             throw new Error(Errors.NOT_FOUND_GUESS_INPUT);
@@ -23,7 +26,7 @@ var handleGuesserGame = function () {
         if (!number) {
             throw new Error(Errors.EMPTY_INPUT);
         }
-        if (number <= config.MIN || number >= config.MAX) {
+        if (number < config.MIN || number > config.MAX) {
             throw new Error(Errors.INVALID_INPUT);
         }
         return number;
@@ -33,10 +36,80 @@ var handleGuesserGame = function () {
             return;
         messageElement.textContent = message;
     };
+    var isValidNumber = function (guessNumber) {
+        if (guessNumber < randomNumber) {
+            setMessage(Errors.NUMBER_TO_LOW);
+            return false;
+        }
+        if (guessNumber > randomNumber) {
+            setMessage(Errors.NUMBER_TO_HIGH);
+            return false;
+        }
+        return true;
+    };
+    var setUI = function () {
+        if (!scoreElement)
+            return;
+        scoreElement.textContent = String(score);
+        if (!livesElement)
+            return;
+        livesElement.textContent = String(lives);
+        if (!highscoreElement)
+            return;
+        highscoreElement.textContent = String(highscore);
+    };
+    var setHighscore = function () {
+        if (highscore <= score) {
+            highscore = score;
+        }
+        score = 0;
+        setUI();
+    };
+    var clearInput = function () {
+        if (!guessInput)
+            return;
+        guessInput.value = "";
+    };
+    var unsuccesfullUI = function () {
+        lives--;
+        setUI();
+    };
+    var succesfullUI = function () {
+        score++;
+        randomNumber = generateRandomNumber(config.MIN, config.MAX);
+        setMessage("Correct!");
+        setUI();
+        clearInput();
+    };
+    var resetGame = function () {
+        score = 0;
+        lives = 5;
+        setMessage("Waiting...");
+        clearInput();
+        setUI();
+    };
+    var messageElement = document.querySelector("[data-message]");
+    var guessInput = document.querySelector("[data-guess]");
+    var checkButton = document.querySelector("[data-check]");
+    var resetButton = document.querySelector("[data-reset]");
+    var scoreElement = document.querySelector("[data-score]");
+    var livesElement = document.querySelector("[data-lives]");
+    var highscoreElement = document.querySelector("[data-highscore]");
+    var randomNumber = generateRandomNumber(config.MIN, config.MAX);
+    var score = 0;
+    var highscore = 0;
+    var lives = 5;
     var guessNumber = function () {
         try {
+            if (lives <= 0) {
+                setHighscore();
+                throw new Error(Errors.END_GAME);
+            }
             var guessNumber_1 = getGuessInputNumber();
-            console.log(guessNumber_1);
+            if (!isValidNumber(guessNumber_1)) {
+                return unsuccesfullUI();
+            }
+            succesfullUI();
         }
         catch (err) {
             if (err instanceof Error) {
@@ -47,5 +120,9 @@ var handleGuesserGame = function () {
     checkButton === null || checkButton === void 0 ? void 0 : checkButton.addEventListener("click", function () {
         guessNumber();
     });
+    resetButton === null || resetButton === void 0 ? void 0 : resetButton.addEventListener("click", function () {
+        resetGame();
+    });
+    setUI();
 };
 handleGuesserGame();
--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,9 @@ enum Errors {
   NOT_FOUND_GUESS_INPUT = "Can't find a guess input 🛑",
   EMPTY_INPUT = "Empty input 🛑",
   INVALID_INPUT = "Accept numbers between 1-20 🛑",
+  NUMBER_TO_LOW = "Too low!",
+  NUMBER_TO_HIGH = "Too high!",
+  END_GAME = "Game just finished!",
 }
 
 const config = {
@@ -10,10 +13,9 @@ const config = {
 };
 
 const handleGuesserGame = () => {
-  const messageElement =
-    document.querySelector<HTMLSpanElement>("[data-message]");
-  const guessInput = document.querySelector<HTMLInputElement>("[data-guess]");
-  const checkButton = document.querySelector<HTMLButtonElement>("[data-check]");
+  const generateRandomNumber = (min: number, max: number) => {
+    return Math.trunc(Math.random() * (max - min) + min);
+  };
 
   const getGuessInputNumber = () => {
     if (!guessInput) {
@@ -30,7 +32,7 @@ const handleGuesserGame = () => {
       throw new Error(Errors.EMPTY_INPUT);
     }
 
-    if (number <= config.MIN || number >= config.MAX) {
+    if (number < config.MIN || number > config.MAX) {
       throw new Error(Errors.INVALID_INPUT);
     }
 
@@ -43,10 +45,99 @@ const handleGuesserGame = () => {
     messageElement.textContent = message;
   };
 
+  const isValidNumber = (guessNumber: number) => {
+    if (guessNumber < randomNumber) {
+      setMessage(Errors.NUMBER_TO_LOW);
+      return false;
+    }
+
+    if (guessNumber > randomNumber) {
+      setMessage(Errors.NUMBER_TO_HIGH);
+      return false;
+    }
+
+    return true;
+  };
+
+  const setUI = () => {
+    if (!scoreElement) return;
+
+    scoreElement.textContent = String(score);
+
+    if (!livesElement) return;
+
+    livesElement.textContent = String(lives);
+
+    if (!highscoreElement) return;
+
+    highscoreElement.textContent = String(highscore);
+  };
+
+  const setHighscore = () => {
+    if (highscore <= score) {
+      highscore = score;
+    }
+
+    score = 0;
+    setUI();
+  };
+
+  const clearInput = () => {
+    if (!guessInput) return;
+
+    guessInput.value = "";
+  };
+
+  const unsuccesfullUI = () => {
+    lives--;
+    setUI();
+  };
+
+  const succesfullUI = () => {
+    score++;
+    randomNumber = generateRandomNumber(config.MIN, config.MAX);
+    setMessage("Correct!");
+    setUI();
+    clearInput();
+  };
+
+  const resetGame = () => {
+    score = 0;
+    lives = 5;
+    setMessage("Waiting...");
+    clearInput();
+    setUI();
+  };
+
+  const messageElement =
+    document.querySelector<HTMLSpanElement>("[data-message]");
+  const guessInput = document.querySelector<HTMLInputElement>("[data-guess]");
+  const checkButton = document.querySelector<HTMLButtonElement>("[data-check]");
+  const resetButton = document.querySelector<HTMLButtonElement>("[data-reset]");
+  const scoreElement = document.querySelector<HTMLElement>("[data-score]");
+  const livesElement = document.querySelector<HTMLElement>("[data-lives]");
+  const highscoreElement =
+    document.querySelector<HTMLElement>("[data-highscore]");
+
+  let randomNumber = generateRandomNumber(config.MIN, config.MAX);
+  let score = 0;
+  let highscore = 0;
+  let lives = 5;
+
   const guessNumber = () => {
     try {
+      if (lives <= 0) {
+        setHighscore();
+        throw new Error(Errors.END_GAME);
+      }
+
       const guessNumber = getGuessInputNumber();
-      console.log(guessNumber);
+
+      if (!isValidNumber(guessNumber)) {
+        return unsuccesfullUI();
+      }
+
+      succesfullUI();
     } catch (err) {
       if (err instanceof Error) {
         setMessage(err.message);
@@ -57,6 +148,12 @@ const handleGuesserGame = () => {
   checkButton?.addEventListener("click", () => {
     guessNumber();
   });
+
+  resetButton?.addEventListener("click", () => {
+    resetGame();
+  });
+
+  setUI();
 };
 
 handleGuesserGame();
